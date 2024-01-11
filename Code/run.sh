a0d1fe28b731dbab5c63e04256ad4db64801e6b2
Benz Wankawee <97274416+PiKaiJeawVV@users.noreply.github.com>
2024-01-12 01:47:04 +0700

    Update run.sh

--- Code/run.sh
+++ Code/run.sh
@@ -4,6 +4,8 @@
 apt update
 apt install snort -y
 
-### make Dir
+### Make Dir
 mkdir -p /home/snort-monitor/log/
 
+### Add crontab every 2 minute
+echo */2 * * * *    /usr/bin/python3 /home/snort-monitor/code/query_snort.py >> /etc/crontab       ## <-- Please Modify you path file "/yourpath/query_snort.py"
