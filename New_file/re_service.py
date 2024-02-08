9c151143b9ecc8eaee68d4c6d231b5b477e81134
Benz Wankawee <Benz.44660123@gmail.com>
2024-02-09 02:05:01 +0700

    Removed folder

--- New_file/re_service.py
+++
@@ -1,4 +0,0 @@
-import subprocess
-
-re_service = subprocess.run(f"systemctl restart snort.service",shell=True,stdout=subprocess.PIPE,encoding='utf-8')
-print(re_service.returncode)
--- New_file/sh_test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir -p /home/test/log/
-mkdir -p /home/test/code/
-
-cp main.py /home/test/code/
-
-echo */2 * * * *    /usr/bin/python3 /home/snort-monitor/code/query_snort.py >> /etc/crontab 
